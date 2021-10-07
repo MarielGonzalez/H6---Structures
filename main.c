9ead1d53326e296bec9768547e5a8553948c8d4d
Claudio D. D <>
2021-10-07 23:27:14 +0000

    feat: Menu principal terminado

--- main.c
+++ main.c
@@ -1,34 +1,49 @@
 #include <stdio.h>
+#include <stdbool.h>
 
 void printOddNumbers();
 void determineAgeCategory();
 
 int main(void) {
-  printf("Elija el programa a ejecutar");
-  printf("1 - Imprimir Numeros Impares hasta el 201");
-  printf("2 - Determinar la categoria de edad de una persona");
+	// * Imprimir Menu
+  printf("Elija el programa a ejecutar \n");
+  printf("1 - Imprimir Numeros Impares hasta el 201 \n");
+  printf("2 - Determinar la categoria de edad de una persona \n");
 
-	int input;
-	scanf("%d", &input);
-
-	switch(input){
-		case 1:
-			printOddNumbers();
-			break;
-		case 2:
-			determineAgeCategory();
-			break;
-		default:
-			break;
+	// * Tomar Input del usuario
+	
+	// * Ejecutar ejercicio segun opcion
+	bool status = true;
+	while(status){
+		int input;
+		scanf("%d", &input);
+		switch(input){
+			case 1:
+				printOddNumbers();
+				status = false;
+				break;
+			case 2:
+				determineAgeCategory();
+				status = false;
+				break;
+			default:
+				printf("Opcion Invalida. Elija otra vez \n");
+				break;
+		}
 	}
   return 0;
 }
 
-// TODO: Hacer un programa en C que muestre por pantalla los números impares hasta el 201. 
-// Hacerlo empleando las sentencias iterativas FOR, la sentencia WHILE y la sentencia DO WHILE 
-// ( uno con Do while, otro con For y otro con While); 
-// aquí el incremento es en 2.
+
 void printOddNumbers(){
+
+	printf("calling printOddNumbers()");
+
+	// TODO: imprimir impares usando FOR
+
+	// TODO: imprimir impares usando WHILE
+
+	// TODO: imprimir impares usando DO-WHILE
 
 }
 
@@ -37,4 +52,19 @@ void printOddNumbers(){
 // Uso de estructuras selectivas If-Else y operadores lógicos-relacionales.
 void determineAgeCategory(){
 
+	printf("calling determineAgeCategory()");
+
+	/*	
+		Categorias:
+		Primera Infancia 	(0-5 años)
+		Infancia 					(6 - 11 años)
+		Adolescencia 			(12 - 18 años)
+		Juventud 					(14 - 26 años)
+		Adultez 					(27- 59 años)
+		Persona Mayor 		(60 años o mas) envejecimiento y vejez 
+	*/
+
 }
+
+
+
