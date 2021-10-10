37db2de077b50210b54e0249a55029d25ed4f557
MarielGonzalez <47929216+MarielGonzalez@users.noreply.github.com>
2021-10-10 17:06:04 +0000

    final

--- main.c
+++ main.c
@@ -13,8 +13,6 @@ int main(void) {
   printf("1 - Imprimir Numeros Impares hasta el 201 \n");
   printf("2 - Determinar la categoria de edad de una persona \n");
 
-	
-	
 	// * Ejecutar ejercicio 1 segun opciones
 	bool status = true;
 	while(status){
@@ -38,70 +36,66 @@ int main(void) {
   return 0;
 }
 
-
 void printOddNumbers(){
 
 char option;
 
 	do{
+		int impar = 1;
+		int i;
+		char eleccion;
+		
+		// TODO: imprimir impares hasta el 201 usando FOR
+		printf("Escoja con cual estructura desea ejecutar el ejercicio:\n");
+		printf("F - SENTENCIA FOR \n");
+		printf("W - SENTENCIA WHILE \n");
+		printf("D - SENTENCIA DO-WHILE \n");
+		
+		scanf("%s", &eleccion);
+		//eleccion=getchar();
 
-	int impar = 1;
-  int i;
-	char eleccion;
+		switch(eleccion){
+			case 'F':
+				// * Sentencia For
+				printf("NUMEROS IMPARES DEL 1 AL 201 CON SENTENCIA FOR \n");
+				for(i=0 ;impar<=201;i++){
+				printf("%d \n",impar);
+				impar+=2; 
+				};
+				break;
+			case 'W':
+				// * Sentencia While
+				printf("NUMEROS IMPARES DEL 1 AL 201 CON SENTENCIA WHILE \n");
+				while(impar<=201) {
+				printf("%d \n",impar);
+				impar+=2;
+				};
+				break;
+
+			case 'D':
+			 	// * Sentencia Do-While
+				do{
+				printf("%d \n",impar);
+				impar+=2;
+					
+				}while(impar<=201); 
+				break;
+
+			default: 
+				printf("opcion no valida");
+		}
+		printf( "Desea continuar....SI(S)/NO(N)?\t \n" );
+		scanf("%s", &option);
 	
+	}while((option='S' && option!='N'));
 
-	// TODO: imprimir impares hasta el 201 usando FOR
-	printf("Escoja con cual estructura desea ejecutar el ejercicio:\n");
-	printf("F - SENTENCIA FOR \n");
-  printf("W - SENTENCIA WHILE \n");
-	printf("D - SENTENCIA DO-WHILE \n");
-   
-  scanf("%s", &eleccion);
-	//eleccion=getchar();
-
- switch(eleccion){
-		case 'F':
-		printf("NUMEROS IMPARES DEL 1 AL 201 CON SENTENCIA FOR \n");
-		for(i=0 ;impar<=201;i++){
-    printf("%d \n",impar);
-    impar+=2; 
-	  };
-		break;
-
-   case 'W':
-   printf("NUMEROS IMPARES DEL 1 AL 201 CON SENTENCIA WHILE \n");
-   while(impar<=201) {
-	 printf("%d \n",impar);
-	 impar+=2;
-	 };
-    break;
-
-  case 'D':
-   do{
-   printf("%d \n",impar);
-   impar+=2;
-    
-  }while(impar<=201); 
-	break;
-
-  default: printf("opcion no valida");
-
- 
-   
-}
-
- printf( "Desea continuar....SI(S)/NO(N)?\t \n" );
- scanf("%s", &option);
- 
-}while((option='S' && option!='N'));
-    printf( "FIN DEL PROGRAMA.\n" );
-    getchar();
- 	 
-
-
+	printf( "FIN DEL PROGRAMA.\n" );
+	getchar();
 }
 
 	
+
+
 void determineAgeCategory(){
 
 	//printf("Tomando en cuenta el siguiente rango de edades:");
@@ -114,52 +108,40 @@ void determineAgeCategory(){
 		Adulto  					(36 - 70 años)
 		Anciano 	      	(70 años o mas)  
 	*/
+	char option;
+	int edad;
+	do{
+		
+		printf( "\n   Introduzca edad: " );
+		scanf( "%du", &edad );
 
+		if ( edad >= 0 && edad <= 122 )
+			if ( edad <= 3 )
+				printf( "La edad ingresada es la de un BEBE \n \t" );
+			else if ( edad <= 11 )
+				printf( "La edad ingresada es la de un NIñO \n \t" );
+			else if ( edad <=18 )
+				printf( "La edad ingresada es la de un ADOLESCENTE \n \t" );					
+			else if ( edad <= 35 && edad>=19 )
+				printf( "La edad ingresada es la de un JOVEN \n \t" );
+			else if ( edad >35 && edad<=70 )
+				printf( "La edad ingresada es la de un ADULTO \n \t" );
+			else
+				printf( "La edad ingresada es la de un ANCIANO \n \t" );									
+		else 
+			printf( "   ERROR: Favor introduzca un margen de edad valido.\n" );
+							
+		getchar();
+		//clrscr ();
+		printf( "Desea continuar....SI(S)/NO(N)?\t \n" );
+		scanf("%s", &option);        
+						
+	}while((option='S' && option!='N'));
 
-char option;
-int edad;
- do{
-  
-    printf( "\n   Introduzca edad: " );
-    scanf( "%du", &edad );
+	printf( "FIN DEL PROGRAMA.\n" );
 
-
-   if ( edad >= 0 && edad <= 122 )
-        if ( edad <= 3 )
-            printf( "La edad ingresada es la de un BEBE \n \t" );
-        else
-            if ( edad <= 11 )
-                printf( "La edad ingresada es la de un NIñO \n \t" );
-            else
-                if ( edad <=18 )
-                   printf( "La edad ingresada es la de un ADOLESCENTE \n \t" );
-                    
-                    else
-                        if ( edad <= 35 && edad>=19 )
-                           printf( "La edad ingresada es la de un JOVEN \n \t" );
-                        else
-                             if ( edad >35 && edad<=70 )
-                             printf( "La edad ingresada es la de un ADULTO \n \t" );
-                             else
-                             printf( "La edad ingresada es la de un ANCIANO \n \t" );
-                    
-        else 
-    	
-            printf( "   ERROR: Favor introduzca un margen de edad valido.\n" );
-            
-             getchar();
-             //clrscr ();
-        
-        
-        printf( "Desea continuar....SI(S)/NO(N)?\t \n" );
-        scanf("%s", &option);        
-          
-        }while((option='S' && option!='N'));
-       printf( "FIN DEL PROGRAMA.\n" );
-
-       
-     
-	}
+	getchar();
+}
 
 
    
